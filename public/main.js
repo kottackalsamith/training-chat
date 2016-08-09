794bcad7cf370eb3559a88a765fd2bf8300fde64
samith <kottackalsamith@gmail.com>
2016-08-09 18:12:44 +0530

    Chat WIthout mongodb working

---
+++ public/main.js
@@ -0,0 +1,24 @@
+// Whole-script strict mode syntax
+"use strict";
+
+var app = angular.module('chatApp', []);
+
+app.factory('socket', function () {
+    var socket = io.connect('http://localhost:3000');
+    return socket;
+});
+
+app.controller('ChatCtrl', function ($scope, socket) {
+    $scope.msgs = [];
+    $scope.sendMsg = function () {
+        socket.emit('send msg', $scope.msg.text);
+         console.log(' angular send message' + $scope.msg.text);
+        $scope.msg.text = '';
+    }
+
+    socket.on('get msg', function(data){
+        console.log(' got message' + data);
+        $scope.msgs.push(data);
+        $scope.$digest();
+    });
+});
--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 // Whole-script strict mode syntax
 "use strict";
 
-var app = require('express')(),;
-    server
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server);
+
+var port = 3000;
+var host = "127.0.0.1";
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));
+io.sockets.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('send msg', function(data){
+        console.log('message' + data);
+        io.sockets.emit('get msg', data);
+    });
+});
+
+var myserver = server.listen(port, host, function () {
+    var host = myserver.address().address;
+    var port = myserver.address().port;
+    console.log('Server running at http://%s:%s', host, port);
+});
